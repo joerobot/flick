ddf71b6da7c28e6f8552a024284676af2b6fa98d
Joe Reed <joe.reed.4@gmail.com>
2016-06-23 18:01:11 +0100

    basic styling. logo positioning

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+var api_key = 'aaa328cd63d691da28ad3adf9a58b5aa';
+
+var params = {
+    method: 'flickr.photos.search',
+    api_key: api_key,
+    tags: search_tag,
+    format: 'json',
+    nojsoncallback: 1
+}
