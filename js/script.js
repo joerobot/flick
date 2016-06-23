014e53f404c141a04338fc3c08db98f36e907065
Joe Reed <joe.reed.4@gmail.com>
2016-06-23 21:46:07 +0100

    hides footer. basic http req

--- js/script.js
+++ js/script.js
@@ -2,21 +2,36 @@ angular.module('search', [])
     .controller('MainCtrl', function($http){
 
         var vm = this;
+
         var api_key = 'aaa328cd63d691da28ad3adf9a58b5aa';
+        var url = 'https://api.flickr.com/services/rest';
+        var search_tag = '';
+
+
+
         vm.submit = function(){
             vm.searched = true;
+            search_tag = vm.userQuery;
+            var params = {
+                method: 'flickr.photos.search',
+                api_key: api_key,
+                tags: search_tag,
+                format: 'json',
+                nojsoncallback: 1,
+            };
+            $http({
+                method: 'GET',
+                url: url,
+                params: params
+            })
+            .then(function(data, status, headers, config){
+                console.log(data.data.photos.photo);
+            });
+
         };
     });
 
 
 
 
-// var url = 'https://api.flickr.com/services/rest';
 //
-// var params = {
-//     method: 'flickr.photos.search',
-//     api_key: api_key,
-//     tags: search_tag,
-//     format: 'json',
-//     nojsoncallback: 1
-// }
